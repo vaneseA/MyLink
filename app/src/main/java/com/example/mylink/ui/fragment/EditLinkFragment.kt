a75abd1fdfd02e81e918131e93710846c56399a0
DK <spol@naver.com>
2023-01-25 22:02:16 +0900

    databinding EditLinkFragment.kt 추가

---
+++ app/src/main/java/com/example/mylink/ui/fragment/EditLinkFragment.kt
@@ -0,0 +1,29 @@
+package com.example.mylink.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.mylink.R
+import com.example.mylink.databinding.FragmentEditLinkBinding
+
+class EditLinkFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding: FragmentEditLinkBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_edit_link,
+            container,
+            false
+        )
+        return binding.root
+    }
+
+
+}
