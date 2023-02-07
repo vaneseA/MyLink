330cf910af6113d9e24dc19429160d8f64216e57
DK <spol@naver.com>
2023-02-07 23:07:36 +0900

    Activity 추가

---
+++ app/src/main/java/com/example/mylink/ui/activity/EditLinkActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mylink.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mylink.R
+
+class EditLinkActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_link)
+    }
+}
